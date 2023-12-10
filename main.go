da67be9ee4b4ce40fa23cce1742f5801ba48c9ca
omcmanus1 <omcmanus1@gmail.com>
2023-12-10 14:58:04 +0000

    add task two

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/omcmanus1/aoc-23/two"
 
 func main() {
-	two.TaskOne()
+	two.TaskTwo()
 }
--- two/02.go
+++ two/02.go
@@ -42,7 +42,7 @@ func TaskOne() {
 
 	scanner := bufio.NewScanner(file)
 	game := 1
-	idTotal := 0
+	total := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -63,10 +63,59 @@ func TaskOne() {
 			}
 		}
 		if !slices.Contains(toAdd, false) {
-			fmt.Println("possible game", "total:", idTotal, "to add:", game)
-			idTotal += game
+			fmt.Println("possible game", "total:", total, "to add:", game)
+			total += game
 		}
 		game++
 	}
-	fmt.Println(idTotal)
+	fmt.Println(total)
+}
+
+/*
+ * Determine minimum number of cubes for each color for a possible game
+ * Multiply red * blue * green min numbers to get the power of each set
+ * Sum each set together for the answer
+ */
+
+func TaskTwo() {
+	file, err := utils.GetFile("two/02-input.txt")
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+			return
+		}
+	}()
+
+	pattern := `(\d+)\s+(red|green|blue)`
+	regex := regexp.MustCompile(pattern)
+
+	scanner := bufio.NewScanner(file)
+	game := 1
+	total := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		groups := strings.Split(line, ";")
+		colorMinimums := map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+		for _, group := range groups {
+			matches := regex.FindAllStringSubmatch(group, -1)
+			for _, match := range matches {
+				color := match[2]
+				if number, err := strconv.Atoi(match[1]); err == nil {
+					if number > colorMinimums[color] {
+						colorMinimums[color] = number
+					}
+				}
+			}
+		}
+		setPower := colorMinimums["red"] * colorMinimums["green"] * colorMinimums["blue"]
+		// fmt.Println("game", game, "minimums:", colorMinimums, "power:", setPower)
+		total += setPower
+		game++
+	}
+	fmt.Println(total)
 }
