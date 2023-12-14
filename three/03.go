1e1188edd4a06e81768a0f794bf9d32766e23550
omcmanus1 <omcmanus1@gmail.com>
2023-12-14 23:26:36 +0000

    refactor

--- three/03.go
+++ three/03.go
@@ -9,7 +9,7 @@ import (
 )
 
 // * search through each line in file
-// * if number is adjacent to a symbol (not period),
+// * if number is adjacent to a symbol (excluding periods),
 // * including horizontal/up/down/diagonal, add to total
 
 func TaskOne() {
@@ -19,45 +19,7 @@ func TaskOne() {
 	}
 	defer file.Close()
 
-	numRegex := regexp.MustCompile(`(\d+)`)
-	symbolRegex := regexp.MustCompile(`[^a-zA-Z0-9\s.]`)
-
-	// indexes of symbol occurrences
-	// one sub-slice per line with all symbol occurrences (multiple or none included)
-	gearIndexes := [][]int{}
-
-	// slice of number occurrences
-	// containing slices of maps (one sub-slice per line, one map per number)
-	// map contains value (single item slice). plus indices [start, end]
-	numIndexes := [][]map[string][]int{}
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		symbolMatches := symbolRegex.FindAllStringIndex(line, -1)
-		tempSymbols := []int{}
-		for _, v := range symbolMatches {
-			tempSymbols = append(tempSymbols, v[0])
-		}
-		gearIndexes = append(gearIndexes, tempSymbols)
-
-		numMatches := numRegex.FindAllStringIndex(line, -1)
-		tempNums := []map[string][]int{}
-		for _, match := range numMatches {
-			matchValue, _ := strconv.Atoi(line[match[0]:match[1]])
-			startIndex := 0
-			if match[0] > 0 {
-				startIndex = match[0] - 1
-			}
-			matchMap := map[string][]int{
-				"value": {matchValue},
-				"range": {startIndex, match[1]},
-			}
-			tempNums = append(tempNums, matchMap)
-		}
-		numIndexes = append(numIndexes, tempNums)
-	}
+	symbolIndexes, numIndexes := populateRefs(`[^a-zA-Z0-9\s.]`, file)
 
 	// loop through array of nums
 	// check previous, current and following line for symbols
@@ -66,20 +28,20 @@ func TaskOne() {
 	partsTotal := 0
 	for lineIndex := range numIndexes {
 		for _, num := range numIndexes[lineIndex] {
-			for _, symb := range gearIndexes[lineIndex] {
+			for _, symb := range symbolIndexes[lineIndex] {
 				if symb >= num["range"][0] && symb <= num["range"][1] {
 					partsTotal += num["value"][0]
 				}
 			}
 			if lineIndex > 0 {
-				for _, symb := range gearIndexes[lineIndex-1] {
+				for _, symb := range symbolIndexes[lineIndex-1] {
 					if symb >= num["range"][0] && symb <= num["range"][1] {
 						partsTotal += num["value"][0]
 					}
 				}
 			}
-			if lineIndex < len(gearIndexes)-1 {
-				for _, symb := range gearIndexes[lineIndex+1] {
+			if lineIndex < len(symbolIndexes)-1 {
+				for _, symb := range symbolIndexes[lineIndex+1] {
 					if symb >= num["range"][0] && symb <= num["range"][1] {
 						partsTotal += num["value"][0]
 					}
@@ -90,6 +52,10 @@ func TaskOne() {
 	fmt.Println("parts total:", partsTotal)
 }
 
+// only search for '*' in lines
+// if '*' is adjacenent to exactly 2 numbers (on surrounding lines)
+// multiply the two numbers and add to total
+
 func TaskTwo() {
 	file, err := os.Open("three/input.txt")
 	if err != nil {
@@ -97,53 +63,15 @@ func TaskTwo() {
 	}
 	defer file.Close()
 
-	numRegex := regexp.MustCompile(`(\d+)`)
-	gearRegex := regexp.MustCompile(`\*`)
-
-	gearIndexes := [][]int{}
-	numIndexes := [][]map[string][]int{}
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		symbolMatches := gearRegex.FindAllStringIndex(line, -1)
-		tempSymbols := []int{}
-		for _, v := range symbolMatches {
-			tempSymbols = append(tempSymbols, v[0])
-		}
-		gearIndexes = append(gearIndexes, tempSymbols)
-
-		numMatches := numRegex.FindAllStringIndex(line, -1)
-		tempNums := []map[string][]int{}
-		for _, match := range numMatches {
-			matchValue, _ := strconv.Atoi(line[match[0]:match[1]])
-			startIndex := 0
-			if match[0] > 0 {
-				startIndex = match[0] - 1
-			}
-			matchMap := map[string][]int{
-				"value": {matchValue},
-				"range": {startIndex, match[1]},
-			}
-			tempNums = append(tempNums, matchMap)
-		}
-		numIndexes = append(numIndexes, tempNums)
-	}
-	// fmt.Println(numIndexes)
-	fmt.Println(gearIndexes)
-
-	// loop through array of nums
-	// check previous, current and following line for symbols
-	// if symbol is covered by index range (plus neighbours), add to total
+	symbolIndexes, numIndexes := populateRefs(`\*`, file)
 
 	partsTotal := 0
-	for gearInd, gearVals := range gearIndexes {
-		if len(gearIndexes[gearInd]) == 0 {
+	for gearInd, gearVals := range symbolIndexes {
+		if len(symbolIndexes[gearInd]) == 0 {
 			continue
 		}
 		for _, gearVal := range gearVals {
-			// gearVal := gearIndexes[gearInd][0]
+			// gearVal := symbolIndexes[gearInd][0]
 			adjacentCount := []int{}
 			fmt.Println("line:", gearInd, "gear index:", gearVal)
 			for _, num := range numIndexes[gearInd] {
@@ -175,4 +103,47 @@ func TaskTwo() {
 	}
 
 	fmt.Println("parts total:", partsTotal)
+}
+
+func populateRefs(symbolPattern string, file *os.File) ([][]int, [][]map[string][]int) {
+	numRegex := regexp.MustCompile(`(\d+)`)
+	symbolRegex := regexp.MustCompile(`[^a-zA-Z0-9\s.]`)
+
+	// indexes of symbol occurrences
+	// one sub-slice per line with all symbol occurrences (multiple or none included)
+	symbolIndexes := [][]int{}
+
+	// slice of number occurrences
+	// containing slices of maps (one sub-slice per line, one map per number)
+	// map contains value (single item slice). plus indices [start, end]
+	numIndexes := [][]map[string][]int{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		symbolMatches := symbolRegex.FindAllStringIndex(line, -1)
+		tempSymbols := []int{}
+		for _, v := range symbolMatches {
+			tempSymbols = append(tempSymbols, v[0])
+		}
+		symbolIndexes = append(symbolIndexes, tempSymbols)
+
+		numMatches := numRegex.FindAllStringIndex(line, -1)
+		tempNums := []map[string][]int{}
+		for _, match := range numMatches {
+			matchValue, _ := strconv.Atoi(line[match[0]:match[1]])
+			startIndex := 0
+			if match[0] > 0 {
+				startIndex = match[0] - 1
+			}
+			matchMap := map[string][]int{
+				"value": {matchValue},
+				"range": {startIndex, match[1]},
+			}
+			tempNums = append(tempNums, matchMap)
+		}
+		numIndexes = append(numIndexes, tempNums)
+	}
+	return symbolIndexes, numIndexes
 }
